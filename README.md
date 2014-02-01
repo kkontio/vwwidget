50261a829eebd939d11e00c8f30cff38b5ddab3a
Kai Kontio <kai.kontio@gmail.com>
2014-02-01 16:59:59 +0200

    Pre GitHub cleanup.

---
+++ README.md
@@ -0,0 +1,10 @@
+Viivi ja Wagner widget
+======================
+
+### Introduction
+
+Viivi ja Wagner widget is an OS X dashboard application, that fetches Viivi ja Wagner comic strip from http://www.hs.fi/ 
+
+### Viivi ja Wagner
+
+Viivi ja Wagner is protected by copyright for Juba Tuomola.
--- Viivi ja Wagner.wdgt/viivijawagner.js
+++ Viivi ja Wagner.wdgt/viivijawagner.js
@@ -2,16 +2,34 @@
     Viivi ja Wagner widget - Fetches Viivi ja Wagner comic strip
     from <http://www.hs.fi/>
     
-    --Mursu 
+    Viivi ja Wagner Copyright (c) Juba Tuomola
+    
+    Project home: https://github.com/kkontio/vwwidget
+    
+    The MIT License (MIT)
 
-    Viivi ja Wagner widget is distributed in the hope that it will
-    be useful, but WITHOUT ANY WARRANTY; without even the implied
-    warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+    Copyright (c) 2014 Kai Kontio
 
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
 */
 
 widget.onshow = onshow;
-var prev_url, next_url;
 var base_url = "http://www.hs.fi";
 
 function onshow() {
@@ -23,28 +41,30 @@ function load_comic(strip_url) {
 }
 
 function parse_data(data) {
-    current_img_url = $(data).find("#full-comic > div + div + div > img").attr("src");
-    prev_url = base_url + $(data).find(".prev-cm").attr("href");
-    next_url = base_url + $(data).find(".next-cm").attr("href");    
+    var current_img_url = $(data).find("#full-comic > div + div + div > img").attr("src");
+    var prev_url = base_url + $(data).find(".prev-cm").attr("href");
+    var next_url = base_url + $(data).find(".next-cm").attr("href");    
     
     $("#viivijawagner").attr("src", current_img_url);
-
+    
+    var prev_arrow = $("#previous");
+    var next_arrow = $("#next");
+    
     //Check if previous comics exist and hide controls if not
     if (prev_url !== "http://www.hs.fi/viivijawagner/") {
-        $("#previous").off("click");
-        $("#previous").on("click", function() { load_comic(prev_url); });
-        $("#previous").show();
+        prev_arrow.off("click");
+        prev_arrow.on("click", function() { load_comic(prev_url); });
+        prev_arrow.show();
     } else {
-        $("#previous").off("click");
-        $("#previous").hide();
+        prev_arrow.off("click");
+        prev_arrow.hide();
     }
     if (next_url !== "http://www.hs.fi/viivijawagner/") {
-        $("#next").off("click");
-        $("#next").on("click", function() { load_comic(next_url); });
-        $("#next").show();
+        next_arrow.off("click");
+        next_arrow.on("click", function() { load_comic(next_url); });
+        next_arrow.show();
     } else {
-        $("#next").off("click");
-        $("#next").hide();
+        next_arrow.off("click");
+        next_arrow.hide();
     }
 }
-
